758318b228206485dc58ab86376d57f5b3be3461
haenfry <haenfry@gmail.com>
2025-01-16 17:07:19 +0000

    fix: Replaced tensorflow.contrib.framework by tf_slim

--- tfsnippet/layers/normalization/weight_norm_.py
+++ tfsnippet/layers/normalization/weight_norm_.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.contrib.framework import add_arg_scope
+from tf_slim import add_arg_scope
 
 from tfsnippet.utils import (ParamSpec,
                              get_static_shape,
